eca6f33bb20c4c967f652d5d7f16a7f2ee4dfd3c
Ephrem <ephrem@Ephrems-MBP.lan>
2023-05-29 14:12:47 -0400

    init

---
+++ noFoodWaste-first/src/main/java/com/noFoodWaste/model/User.java
@@ -0,0 +1,115 @@
+package com.noFoodWaste.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User
+{
+    private static final long serialVersionUID = 1L;
+
+    public User( String name, String email, String password, List<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Role> roles = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getName(), user.getName()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPassword(), user.getPassword()) && Objects.equals(getRoles(), user.getRoles());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail(), getPassword(), getRoles());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
---
+++ noFoodWaste-first/src/main/java/com/noFoodWaste/service/CustomUserDetailsService.java
@@ -0,0 +1,79 @@
+package com.noFoodWaste.service;
+//
+//import com.noFoodWaste.model.Role;
+//import com.noFoodWaste.model.User;
+//import com.noFoodWaste.repository.UserRepository;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import java.util.Collection;
+//import java.util.stream.Collectors;
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private UserRepository userRepository;
+//
+//    public CustomUserDetailsService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        User user = userRepository.findByEmail(email);
+//
+//        if (user != null) {
+//            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+//                    user.getPassword(),
+//                    mapRolesToAuthorities(user.getRoles()));
+//        }else{
+//            throw new UsernameNotFoundException("Invalid username or password.");
+//        }
+//    }
+//
+//    private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+//        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
+//                .map(role -> new SimpleGrantedAuthority(role.getName()))
+//                .collect(Collectors.toList());
+//        return mapRoles;
+//    }
+//}
+
+
+
+
+
+import com.noFoodWaste.model.User;
+import com.noFoodWaste.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail()
+                    , user.getPassword(),
+                    user.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList()));
+        } else {
+            throw new UsernameNotFoundException("Invalid email or password");
+        }
+    }
+}
