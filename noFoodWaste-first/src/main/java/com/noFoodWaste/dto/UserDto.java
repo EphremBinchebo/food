ffa598c83b94ec4ede51a4357f5186f77d7c83ac
Binchebo <961760@cognizant.com>
2023-05-31 18:44:39 -0400

    init

--- noFoodWaste-first/src/main/java/com/noFoodWaste/dto/UserDto.java
+++ noFoodWaste-first/src/main/java/com/noFoodWaste/dto/UserDto.java
@@ -3,9 +3,9 @@ package com.noFoodWaste.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+//import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+//import lombok.Setter;
 
 //@Getter
 //@Setter
